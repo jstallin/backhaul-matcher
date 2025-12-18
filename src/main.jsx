c9f19609ab5b3b752325f7c18944cdeba639de72
jsstallings <js.stallings@gmail.com>
2025-12-18 11:53:33 -0500

    Modifying Data

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from './App.jsx'
 import './index.css'
 import { AuthProvider } from './contexts/AuthContext'
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
